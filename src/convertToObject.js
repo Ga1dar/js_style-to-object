f43954c1f4bb0274308a87fa1d6517eace976aa2
Kyrylo Holikov <pipecc1985@gmail.com>
2024-11-04 22:06:34 +0100

    test task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleArray = sourceString.split(';');
+  const styleObject = {};
+
+  styleArray.forEach((style) => {
+    if (style.trim()) {
+      const [key, ...valueParts] = style.split(':');
+
+      if (valueParts.length > 0) {
+        const value = valueParts.join(':').trim();
+
+        styleObject[key.trim()] = value;
+      }
+    }
+  });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;
