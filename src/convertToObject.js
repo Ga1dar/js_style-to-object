3bc429518e872e5dd6ab2e7597be3dece27d8e49
Kyrylo Holikov <pipecc1985@gmail.com>
2024-11-04 22:35:02 +0100

    check after inspection

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,6 +6,10 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
+  if (typeof sourceString !== 'string') {
+    return {};
+  }
+
   const styleArray = sourceString.split(';');
   const styleObject = {};
 
@@ -13,7 +17,7 @@ function convertToObject(sourceString) {
     if (style.trim()) {
       const [key, ...valueParts] = style.split(':');
 
-      if (valueParts.length > 0) {
+      if (key.trim() && valueParts.length > 0) {
         const value = valueParts.join(':').trim();
 
         styleObject[key.trim()] = value;
